b3d0b9a299ec2d034555d40394cd8b6c3e69a77f
mrfergos <lyzenroma33@gmail.com>
2025-02-12 17:40:53 +0300

    Update ir_nec.ino

--- M5stickCPlus2/randomGraph/ir_nec/ir_nec.ino
+++ M5stickCPlus2/randomGraph/ir_nec/ir_nec.ino
@@ -1,18 +1,24 @@
 /**
- * @file ir_nec.ino
- * @author SeanKwok (shaoxiang@m5stack.com)
- * @brief M5StickCPlus2 IR NEC test
- * @version 0.1
- * @date 2023-12-09
- *
- *
- * @Hardwares: M5StickCPlus2
- * @Platform Version: Arduino M5Stack Board Manager v2.0.9
- * @Dependent Library:
- * M5GFX: https://github.com/m5stack/M5GFX
- * M5Unified: https://github.com/m5stack/M5Unified
- * M5StickCPlus2: https://github.com/m5stack/M5StickCPlus2
- * IRremote: https://github.com/Arduino-IRremote/Arduino-IRremote
+.
+⠄⠄⠄⠸⣿⡛⠷⣦
+⢀⣄⠄⠄⠈⠻⣦⡀⢙⣤
+⢸⡟⢷⣄⠄⠄⣨⡷⠄⢻⡇
+⠘⣷⡀⠙⠷⠾⠋⠄⠄⠺⣧⣀⣴⠶⢶⡄
+ ⠈⠻⢶⣤⣤⣤⣄⣀⣀⣼⠟⠁⠄⢠⣿⠶⣦⡀
+       ⠈⢩⡿⠋⠙⠛⠷⣦⣴⣟⠁⠄⣸⣧⣤⣄
+      ⣰⡟⠁⠄⠄⠄⠄⠄⠉⠛⢷⣾⠋⠄⠄⣿⣤⣄⡀
+     ⣼⠏⠄⠄⠄⣠⡶⠶⣶⣤⣤⣾⡃⠄⣠⡾⠋⠄⢙⣷
+     ⢿⡄⠄⠄⠄⠉⠁⠄⠈⠻⣿⣌⠻⢿⣿⠄⠄⣠⣾⡇
+     ⠈⢿⣆⠄⠄⠄⠄⠄⠄⠄⠈⠻⣷⡀⠻⠷⠾⢿⣼⣧
+        ⠙⣷⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣧⡀⠄⠄⠙⠛⠛⠛⠷⣦⡀
+          ⢿⣄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢻⣦⠄⠄⣠⡶⢷⣄⠈⢿⡄
+           ⠹⣧⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⠄⢼⡋⠄⠄⠙⢷⣼⡇
+            ⢹⡇⠄⠄⠄⠄⠄⠄⠄⠄⢻⣆⠈⠻⣦⡀⠄⠄⠙⠃
+            ⢸⡇⠄⠄⠄⠄⠄⠄⠄⠄⢸⡟⠻⣶⣬⣿⡆
+           ⢠⡾⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⣧⡀
+           ⣿   ЫЫЫ,       ⢸⡇
+           ⣿              ⢸⡇
+           ⠿  Мы профи    ⠸⠇
  * 
  * 
  * 
